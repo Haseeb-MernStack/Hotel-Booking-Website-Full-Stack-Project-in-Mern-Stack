0f5481e3456a846c958012e95620f70a11c30a2d
Haseeb MernStack <seebpubg@gmail.com>
2025-05-07 13:55:19 +0500

    Updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -41,21 +41,20 @@ const Navbar = () => {
   const location = useLocation();
 
   useEffect(() => {
-
-    if(location.pathname !== "/"){
+    if (location.pathname !== "/") {
       setIsScrolled(true);
       return;
-    }else{
+    } else {
       setIsScrolled(false);
     }
-    setIsScrolled(prev => location.pathname !== '/' ? true : prev);
+    setIsScrolled((prev) => (location.pathname !== "/" ? true : prev));
 
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 10);
     };
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  }, [location.pathname ]);
+  }, [location.pathname]);
 
   return (
     <nav
